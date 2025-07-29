9f888edb5cff9cd9f1646f4e85ee0cf63df406f8
KwdeTfpv <simon_995@163.com>
2025-07-29 17:07:50 +0800

    优化：在 build.rs 中增加 GIT_TAG 警告输出

--- build.rs
+++ build.rs
@@ -7,4 +7,5 @@ fn main() {
         .and_then(|o| String::from_utf8(o.stdout).ok())
         .unwrap_or_else(|| "unknown".to_string());
     println!("cargo:rustc-env=GIT_TAG={}", tag.trim());
+    println!("cargo:warning=GIT_TAG={}", tag.trim());
 }
